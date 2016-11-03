939c2011aafba7455fea4cc8f179d24f65b133b5
carlos-valer <carlos.valer@ucsp.edu.pe>
2016-11-03 09:37:35 -0500

    poligono

--- clases/main.cpp
+++ clases/main.cpp
@@ -20,8 +20,17 @@ void printAtributos(Poligono *polygon)
 
 
 int main () {
-	Point point1(0,0),point2(0,4),point3(5,0);
-	Point p1,p2(5,4);
+	int t1x,t1y,t2x,t2y,t3x,t3y;
+	int n1x,n1y,n2x,n2y;
+	cout<<"punto inferior izquierdo: ";
+	cin>>n1x>>n1y;
+	cout<<"\npunto inferior izquierdo: ";
+	cin>> n2x>>n2y;
+	cout<<"\puntos del triangulo: ";
+	cin>> t1x>>t1y>>t2x>>t2y>>t3x>>t3y;
+	
+	Point point1(t1x,t1y),point2(t2x,t2y),point3(t3x,t3y);
+	Point p1(n1x,n1y),p2(n2x,n2y);
 	
 	Triangulo t(point1,point2,point3);
 	Rectangulo rect(p1,p2);
